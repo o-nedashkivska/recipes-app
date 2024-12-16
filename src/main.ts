d67d2fc42c1fa3e174281c559322c60dd7cbbb22
Oleksandra Nedashkivska <oleksandra.nedashkivska@binary-studio.com>
2024-12-16 14:33:51 +0200

    chore: update project structure

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import Vue from "vue";
 
 import App from "./App.vue";
-import router from "./router";
+// import router from "./router";
 
 import "./assets/main.css";
 
 new Vue({
-  router,
+  // router,
   render: (h) => h(App),
 }).$mount("#app");
